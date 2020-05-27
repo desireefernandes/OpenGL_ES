f55076c70edc57df8252d18c0a04ee2470618bb4
desireefernandes <desireefernandes00@gmail.com>
2020-05-27 19:33:08 -0300

    OpenGL_Colors - Commit inicial

---
+++ OpenGL_Colors/app/src/main/java/com/example/opengl_colors/MyGLRenderer.java
@@ -0,0 +1,87 @@
+package com.example.opengl_colors;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MyGLRenderer implements GLSurfaceView.Renderer {
+    private Shape mShape;
+
+    private float[] rotationMatrix = new float[16];
+
+    private final float[] vPMatrix = new float[16];
+    private final float[] projectionMatrix = new float[16];
+    private final float[] viewMatrix = new float[16];
+
+    public volatile float mAngle;
+
+    public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+        mShape = new Shape(
+                new float[]{
+                        // X, Y, Z,
+                        // R, G, B, A
+                        -0.5f, -0.5f, 0.0f,
+                        1.0f, 1.0f, 0.0f, 1.0f,
+
+                        0.5f, -0.5f, 0.0f,
+                        0.0f, 1.0f, 1.0f, 1.0f,
+
+                        0.5f, 0.5f, 0.0f,
+                        1.0f, 0.0f, 1.0f, 1.0f
+                }
+        );
+
+    }
+
+    public void onDrawFrame(GL10 unused) {
+        GLES20.glClearColor(0.5f, 0.5f, 0.5f, 0.5f);
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+        float[] scratch = new float[16];
+
+        //long time = SystemClock.uptimeMillis() % 4000L;
+        //float angle = 0.090f * ((int) time);
+
+        //Matrix.setRotateM(rotationMatrix, 0, angle, 0, 0, -1.0f);
+        Matrix.setRotateM(rotationMatrix, 0, mAngle, 0, 0, -1.0f);
+        Matrix.setLookAtM(viewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+        Matrix.multiplyMM(scratch, 0, vPMatrix, 0, rotationMatrix, 0);
+
+        mShape.draw(scratch);
+
+    }
+
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+
+    }
+
+    public static int loadShader(int type, String shaderCode) {
+
+        int shader = GLES20.glCreateShader(type);
+
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
+    }
+
+    public float getAngle() {
+        return mAngle;
+    }
+
+    public void setAngle(float angle) {
+        mAngle = angle;
+    }
+
+}
---
+++ OpenGL_Colors/app/src/main/java/com/example/opengl_colors/Shape.java
@@ -0,0 +1,168 @@
+package com.example.opengl_colors;
+
+import android.opengl.GLES20;
+import android.opengl.Matrix;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+public class Shape {
+    private final FloatBuffer vertexBuffer;
+
+    private int mPositionHandle;
+    private int colorHandle;
+    private int vPMatrixHandle;
+
+    private final int bytesPerFloat = 4;
+    private final int strideBytes = 7 * bytesPerFloat;
+    private final int positionOffset = 0;
+    private final int positionDataSize = 3;
+    private final int colorOffset = 3;
+    private final int colorDataSize = 4;
+
+    private float shapeCoords[] = new float[21];
+    public void setShapeCoords(float[] shapeCoords) {
+        this.shapeCoords = shapeCoords;
+    }
+
+    public Shape(float[] shapeCoords) {
+        this.setShapeCoords(shapeCoords);
+
+        vertexBuffer = ByteBuffer.allocateDirect(
+                shapeCoords.length * bytesPerFloat)
+                .order(ByteOrder.nativeOrder()).asFloatBuffer();
+
+        vertexBuffer.put(shapeCoords).position(0);
+
+    }
+
+    public void draw(float[] mvpMatrix) {
+        int vertexShader = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
+
+        if (vertexShader != 0)
+        {
+            GLES20.glShaderSource(vertexShader, vertexShaderCode);
+            GLES20.glCompileShader(vertexShader);
+
+            final int[] compileStatus = new int[1];
+            GLES20.glGetShaderiv(vertexShader, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+            if (compileStatus[0] == 0)
+            {
+                GLES20.glDeleteShader(vertexShader);
+                vertexShader = 0;
+            }
+        }
+
+        if (vertexShader == 0)
+        {
+            throw new RuntimeException("Error creating vertex shader.");
+        }
+
+        int fragmentShader = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
+
+        if (fragmentShader != 0)
+        {
+            GLES20.glShaderSource(fragmentShader, fragmentShaderCode);
+            GLES20.glCompileShader(fragmentShader);
+
+            final int[] compileStatus = new int[1];
+            GLES20.glGetShaderiv(fragmentShader, GLES20.GL_COMPILE_STATUS, compileStatus, 0);
+
+            if (compileStatus[0] == 0)
+            {
+                GLES20.glDeleteShader(fragmentShader);
+                fragmentShader = 0;
+            }
+        }
+
+        if (fragmentShader == 0)
+        {
+            throw new RuntimeException("Error creating fragment shader.");
+        }
+
+        int programHandle = GLES20.glCreateProgram();
+
+        if (programHandle != 0)
+        {
+            // Bind the vertex shader to the program.
+            GLES20.glAttachShader(programHandle, vertexShader);
+
+            // Bind the fragment shader to the program.
+            GLES20.glAttachShader(programHandle, fragmentShader);
+
+            // Bind attributes
+            GLES20.glBindAttribLocation(programHandle, 0, "a_Position");
+            GLES20.glBindAttribLocation(programHandle, 1, "a_Color");
+
+            // Link the two shaders together into a program.
+            GLES20.glLinkProgram(programHandle);
+
+            // Get the link status.
+            final int[] linkStatus = new int[1];
+            GLES20.glGetProgramiv(programHandle, GLES20.GL_LINK_STATUS, linkStatus, 0);
+
+            // If the link failed, delete the program.
+            if (linkStatus[0] == 0)
+            {
+                GLES20.glDeleteProgram(programHandle);
+                programHandle = 0;
+            }
+        }
+
+        if (programHandle == 0)
+        {
+            throw new RuntimeException("Error creating program.");
+        }
+
+        vPMatrixHandle = GLES20.glGetUniformLocation(programHandle, "u_MVPMatrix");
+        mPositionHandle = GLES20.glGetAttribLocation(programHandle, "a_Position");
+        colorHandle = GLES20.glGetAttribLocation(programHandle, "a_Color");
+
+        GLES20.glUseProgram(programHandle);
+
+        vertexBuffer.position(positionOffset);
+        GLES20.glVertexAttribPointer(mPositionHandle, positionDataSize, GLES20.GL_FLOAT, false,
+                strideBytes, vertexBuffer);
+
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+
+        vertexBuffer.position(colorOffset);
+        GLES20.glVertexAttribPointer(colorHandle, colorDataSize, GLES20.GL_FLOAT, false,
+                strideBytes, vertexBuffer);
+
+        GLES20.glEnableVertexAttribArray(colorHandle);
+
+        GLES20.glUniformMatrix4fv(vPMatrixHandle, 1, false, mvpMatrix, 0);
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, 3);
+    }
+
+    private final String vertexShaderCode =
+            "uniform mat4 u_MVPMatrix;      \n"
+
+                    + "attribute vec4 a_Position;     \n"
+                    + "attribute vec4 a_Color;        \n"
+
+                    + "varying vec4 v_Color;          \n"
+
+                    + "void main()                    \n"
+                    + "{                              \n"
+                    + "   v_Color = a_Color;          \n"
+
+                    + "   gl_Position = u_MVPMatrix   \n"
+                    + "               * a_Position;   \n"
+                    + "}                              \n";
+
+    private final String fragmentShaderCode =
+            "precision mediump float;       \n"
+
+                    + "varying vec4 v_Color;          \n"
+
+                    + "void main()                    \n"
+                    + "{                              \n"
+                    + "   gl_FragColor = v_Color;     \n"
+                    + "}";
+
+}
